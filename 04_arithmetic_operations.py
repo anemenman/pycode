57d466eacb0f30a4de8c931f66ef21d3ef9ad160
A A <aa@MacBook-Pro-A.local>
2020-08-05 14:55:29 +0300

    Arithmetic operations

---
+++ 04_arithmetic_operations.py
@@ -0,0 +1,18 @@
+# whole part of division
+import math
+
+print(10 // 3)  # ...=3
+
+# Modulo operation
+print(10 % 3)  # ... =1
+
+# Exponentiation
+print(5 ** 2)
+
+# complex
+print(1 + 5j)
+print(complex(2, 7))
+
+# math
+print(math.factorial(5))  # 1*2*3*4*5
+print(1 * 2 * 3 * 4 * 5)
